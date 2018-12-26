a929e098f4ab3cf4fe2d2bd7b272b64c6798eb33
archanadangi04 <45931886+archanadangi04@users.noreply.github.com>
2018-12-26 16:40:11 +0530

    Add files

---
+++ params.py
@@ -0,0 +1,6 @@
+organisations = {
+    'Accenture': {'api_key': 'e8077537b6a6d5e0d3bc0c89978842bf',
+    'app_key': 'a44b88845c077400cb2e75f55824090f0259de4d'},
+    'FCA': {'api_key': 'e8077537b6a6d5e0d3bc0c89978842bf',
+    'app_key': 'a44b88845c077400cb2e75f55824090f0259de4d'}
+}
