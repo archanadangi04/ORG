6ad16f77fc409c0efe834fb9a35a9f4042e01717
archanadangi04 <45931886+archanadangi04@users.noreply.github.com>
2019-01-18 10:14:20 +0000

    MainScript

---
+++ mainscript.py
@@ -0,0 +1,45 @@
+from datadog import initialize, api
+	import csv
+	import requests
+	import re
+	from params import organisations
+	
+
+	path = "/tmp/datadog.csv"
+	
+
+	deflist = [["datadog_organization","name","email","handle","is_admin","is_disabled","is_verified","role","access_role"]]
+	with open('orgnames.txt', 'r') as readFile:
+	    data = readFile.readlines()
+	
+
+	for i in range(len(data)):
+	    data[i] = data[i].replace('\n','')
+	
+
+	for i in data:
+	    orgvalue = organisations.get(i)
+	    api_key = orgvalue.get('api_key')
+	    app_key = orgvalue.get('app_key')
+	    url = "https://api.datadoghq.com/api/v1/org?api_key=%s&application_key=%s" %(api_key,app_key)
+	    resp = requests.get(url)
+	    raworgdetails = resp.content
+	    raworgdetails = raworgdetails.decode('utf-8')
+	    orgdetails = re.search(r'"name":(.+?")', raworgdetails)
+	    orgname = orgdetails.group().split(':')[1].strip('"')
+	    initialize(**orgvalue)
+	    rawdata = api.User.get_all()
+	    userlist = rawdata.get('users')
+	    list1 = []
+	    list1.append(orgname)
+	    for i in range(len(userlist)):
+	       for j in ("name","email","handle","is_admin","disabled","verified","role","access_role"):
+	          list1.append(userlist[i].get(j))
+	       deflist.append(list1)
+	       list1 = []
+	#    print(deflist)
+	    with open(path, 'w') as writeFile:
+	        writer = csv.writer(writeFile)
+	        writer.writerows(deflist)
+	    writeFile.close()
+
